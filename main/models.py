cae7eae2b7143b1c03a4e4093baac6b98eeb1c3e
GJLIB <glebnazarov.2008@gmail.com>
2026-02-12 20:38:02 +0200

    +models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва страви")
+    description = models.TextField(blank=True, verbose_name="Опис")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Ціна")
+    available = models.BooleanField(default=True, verbose_name="Доступна")
+    image = models.ImageField(upload_to='menu_images', null=True, blank=True,verbose_name="Зображення")
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100, verbose_name="Ім'я клієнта")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    created_at = models.DateTimeField(auto_now_add=True)
+    address = models.TextField(default='')
+
+    def __str__(self):
+        return f"Замовлення №{self.id} - {self.customer_name}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.menu_item.name} x {self.quantity}"
